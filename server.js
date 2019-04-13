c544e21ca1cef44c2889bddb68dc6181b2484d86
Jacob Angulo <jacobangulo@hotmail.com>
2019-04-12 22:42:10 -0700

    data schema created

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const DishesRouter = require("./routes/dishes-router");
 
 const server = express();
+
+server.use("/api/dishes", DishesRouter);
 
 server.get("/", async (req, res) => {
   res.send("welcome to the '/'");
